61822b6add14455bb43a66b3ce6146dcfd06521b
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-08-04 12:59:49 +0600

    firebase upload

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/Routes.jsx';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='maximum-width'>
     <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
   </React.StrictMode>,
   </div>
 )
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,107 @@
+
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaGoogle} from "react-icons/fa";
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
+const Register = () => {
+    const [error,setError] = useState('');
+    const {createUser,logOut,userProfile,googleLogin} = useContext(AuthContext);
+    const [accepted,setAccepted] = useState(false)
+    const navigate = useNavigate();
+    const handleRegister = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        // console.log(name,photo,email,password)
+        createUser(email,password)
+        .then(result=>{
+            console.log(result.user)
+            setError('')
+              userProfile(name,photo)
+              .then(r=>console.log(r))
+              .catch(error=>console.log(error))
+              logOut();
+              navigate('/login');
+              
+        
+        })
+        .catch(error=>{
+            setError(error.message)
+        })
+        
+        form.reset();
+    }
+    const handleAccepted = event =>{
+        setAccepted(event.target.checked)
+    }
+    const handleGoogleLogin = ()=>{
+        googleLogin()
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser)
+            navigate('/')
+        })
+        .catch(error=>console.log(error))
+    }
+    
+    return (
+        <Container className='w-25 mx-auto pb-5'>
+
+            <h3>Please Register</h3>
+            <Form onSubmit = {handleRegister}>
+                <Form.Group className="mb-1" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" name="name" placeholder="Your name" required />
+
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="formBasicEmail">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name="photo" placeholder="Photo URL" required />
+
+                </Form.Group>
+                <Form.Group className="mb-1" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
+
+                </Form.Group>
+
+                <Form.Group className="mb-1" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" required/>
+                </Form.Group>
+                <Form.Group className="mb-1 text-dark" controlId="formBasicCheckbox">
+                    <Form.Check
+                    onClick={handleAccepted}
+                     type="checkbox"
+                     name="accept"
+                     label={<>Accept <Link  to='/terms'>Terms & Conditions</Link></>} />
+                </Form.Group>
+                <Button disabled={!accepted} variant="dark" type="submit">
+                    Register
+                </Button>
+                <br />
+                <Form.Text className="text-secondary">
+                    Already Have an Account <Link to="/login">Login</Link>
+                </Form.Text>
+                <Form.Text className="text-success">
+
+                </Form.Text>
+                <Form.Text className="text-danger">
+
+                </Form.Text>
+            </Form>
+            <p className='text-danger'>{error}</p>
+            <Button onClick={handleGoogleLogin} className='mb-2 mt-2' variant="outline-info"> <FaGoogle></FaGoogle> Login with Google</Button>
+                
+                <br />
+                <br />
+
+        </Container>
+    );
+};
+
+export default Register;
